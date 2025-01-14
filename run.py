5c1545d4a4348cc06f45fea252f9183e133c216c
lineshogan <lineshogan@foxmail.com>
2025-01-14 15:07:09 +0800

    update run

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from beir.retrieval import models
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 from beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
-
+import random
 import logging
 import pathlib, os
 
@@ -14,15 +14,36 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
                     handlers=[LoggingHandler()])
 #### /print debug information to stdout
 
-data_path = "/root/beir/fiqa"
+data_path = "/root/beir/nfcorpus"
 
 #### Provide the data_path where scifact has been downloaded and unzipped
 corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 
 #### batch_size = 8 for 32G VRAM
-model = DRES(models.TargetEmbedBEIR("/root/autodl-tmp/qwen2_5_7b", mode="sum"), batch_size=8)
+model = DRES(models.TargetEmbedBEIR("/root/autodl-tmp/qwen2_5_7b", mode="concat"), batch_size=8)
 retriever = EvaluateRetrieval(model, score_function="cos_sim") # or "cos_sim" for cosine similarity
 results = retriever.retrieve(corpus, queries)
 
 #### Evaluate your model with NDCG@k, MAP@K, Recall@K and Precision@K  where k = [1,3,5,10,100,1000] 
 ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+print(ndcg)
+print(_map)
+print(recall)
+print(precision)
+
+import pdb; pdb.set_trace()
+# mrr = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="mrr")
+# recall_cap = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="r_cap")
+# hole = retriever.evaluate_custom(qrels, results, retriever.k_values, metric="hole")
+
+#### Print top-k documents retrieved ####
+# top_k = 10
+
+# query_id, ranking_scores = random.choice(list(results.items()))
+# scores_sorted = sorted(ranking_scores.items(), key=lambda item: item[1], reverse=True)
+# logging.info("Query : %s\n" % queries[query_id])
+
+# for rank in range(top_k):
+#     doc_id = scores_sorted[rank][0]
+#     # Format: Rank x: ID [Title] Body
+#     logging.info("Rank %d: %s [%s] - %s\n" % (rank+1, doc_id, corpus[doc_id].get("title"), corpus[doc_id].get("text")))
